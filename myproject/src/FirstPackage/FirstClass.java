c297fa5f03df867b09a4c7de1de06597860d0cf5
llxSpace <2313374188@qq.com>
2021-07-09 22:22:16 +0800

    eclipse第一次发布项目，在这之前需要add。

---
+++ myproject/src/FirstPackage/FirstClass.java
@@ -0,0 +1,13 @@
+package FirstPackage;
+
+public class FirstClass {
+	public static void main(String [] args){
+	System.out.println("我相信我自己");
+	System.out.println("HelloWorld!");
+	System.out.println("HelloWorld!");
+	System.out.println("HelloWorld!");
+	for(int i = 0; i < 5; i++) {
+		System.out.println("我相信我自己");
+		}
+	}
+}
